bbc706ff063f25f8752c4dcab3fc960714c56578
gtxaspec <gtxent@gmail.com>
2025-02-13 11:06:48 -0800

    board.c: don't print reset failure

--- arch/mips/lib/board.c
+++ arch/mips/lib/board.c
@@ -374,9 +374,7 @@ extern void board_usb_init(void);
 		printf("JZNET:   init failed\n");
 	}
 
-	if (run_command("factory reset", 0) != 0) {
-		printf("RST:   reset failed\n");
-	} else {
+	if (run_command("factory reset", 0) == 0) {
 		printf("RST:   reset successful, resetting system...\n");
 		run_command("reset", 0);
 	}
